d603b8de934d1cfceb85eed29f34bc198252a1e0
朱继超 <984065974@qq.com>
2025-02-14 11:42:57 +0800

    ignore config

--- EaseChatDemo/EaseChatDemo/CustomConstants/PublicDefines.swift
+++ EaseChatDemo/EaseChatDemo/CustomConstants/PublicDefines.swift
@@ -7,12 +7,11 @@
 
 import Foundation
 
-// easemob#easeim
-public let AppKey: String = "easemob#easeim"
-
+//参考环信官方文档
+public let AppKey: String = <#Your AppKey#>
 //最好使用自己的Appserver地址，尽量不要使用此地址，此地址只用于快速跑通Demo,此Demo对应Appserver是开源的具体参看demo readme
-public let ServerHost: String =  "https://a1-appserver.easemob.com"
+public let ServerHost: String = <#Your Appserver Address#>
 
 // https://docs.agora.io/en/agora-chat/get-started/enable?platform=ios
 // Create a new app in the Agora Console, and use the App Id in the following code
-public let CallKitAppId: String = "15cb0d28b87b425ea613fc46f7c9f974"
+public let CallKitAppId: String = <#Your Agora App Id#>
