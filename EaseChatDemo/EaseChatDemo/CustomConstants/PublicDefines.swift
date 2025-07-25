d6f852aaa631d060f40c9666b4f38f81f537b00b
李小明 <527725681@qq.com>
2025-07-25 16:26:17 +0800

    Update PublicDefines.swift

--- EaseChatDemo/EaseChatDemo/CustomConstants/PublicDefines.swift
+++ EaseChatDemo/EaseChatDemo/CustomConstants/PublicDefines.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 //参考环信官方文档
-public let AppKey: String = "easemob#easeim"
+public let AppKey: String = ""
 //最好使用自己的Appserver地址，尽量不要使用此地址，此地址只用于快速跑通Demo,此Demo对应Appserver是开源的具体参看demo readme
 public let ServerHost: String = ""
 
