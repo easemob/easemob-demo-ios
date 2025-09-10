0c619a1ea22858add379232ae02982f3e053de19
朱继超 <984065974@qq.com>
2025-09-10 11:49:16 +0800

    Update PublicDefines.swift

--- EaseChatDemo/EaseChatDemo/CustomConstants/PublicDefines.swift
+++ EaseChatDemo/EaseChatDemo/CustomConstants/PublicDefines.swift
@@ -8,10 +8,7 @@
 import Foundation
 
 //参考环信官方文档
-public let AppKey: String = ""
+public let AppKey: String = <#AppKey#>
 //最好使用自己的Appserver地址，尽量不要使用此地址，此地址只用于快速跑通Demo,此Demo对应Appserver是开源的具体参看demo readme
-public let ServerHost: String = ""
+public let ServerHost: String = <#Your Host#>
 
-// https://docs.agora.io/en/agora-chat/get-started/enable?platform=ios
-// Create a new app in the Agora Console, and use the App Id in the following code
-public let CallKitAppId: String = ""
